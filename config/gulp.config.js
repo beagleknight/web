65072bbdaff0cc4efffe643e757cd58779754674
beagleknight <david.morcillo@gmail.com>
2015-11-13 13:56:59 +0100

    Using read-only template by html5up plus my gulp workflow

--- config/gulp.config.js
+++ config/gulp.config.js
@@ -7,7 +7,45 @@ module.exports = {
         dest: destFolder
     },
     files: {
-        templates: `${sourceFolder}/templates/**/*.html`
+        templates: `${sourceFolder}/templates/**/*.html`,
+        styles: {
+            libs: [
+                `${sourceFolder}/styles/font-awesome.min.css`
+            ],
+            main: `${sourceFolder}/styles/index.scss`
+        },
+        images: `${sourceFolder}/images/**.*`,
+        scripts: {
+            libs: [
+                `${sourceFolder}/scripts/jquery.min.js`,
+                `${sourceFolder}/scripts/jquery.scrollzer.min.js`,
+                `${sourceFolder}/scripts/jquery.scrolly.min.js`,
+                `${sourceFolder}/scripts/skel.min.js`,
+                `${sourceFolder}/scripts/util.js`
+            ],
+            main: [
+                `${sourceFolder}/scripts/index.js`
+            ]
+        },
+        fonts: `${sourceFolder}/fonts/**.*`
+    },
+    styles: {
+        destFolder: `${destFolder}/css`
+    },
+    images: {
+        destFolder: `${destFolder}/images`
+    },
+    scripts: {
+        destFolder: `${destFolder}/js`,
+        libs: {
+            outFile: `libs.js`
+        },
+        main: {
+            outFile: `index.js`
+        }
+    },
+    fonts: {
+        destFolder: `${destFolder}/fonts`
     },
     server: {
         root: destFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp   = require('gulp'),
     $      = require('gulp-load-plugins')({ lazy: true }),
+    env    = process.env.NODE_ENV || 'development',
     config = require('./config/gulp.config');
 
 gulp.task('templates', function () {
@@ -9,10 +10,63 @@ gulp.task('templates', function () {
         .pipe($.connect.reload());
 });
 
-gulp.task('build', ['templates']);
+gulp.task('styles:libs', function () {
+    gulp.src(config.files.styles.libs)
+        .pipe($.plumber())
+        .pipe(gulp.dest(config.styles.destFolder))
+        .pipe($.connect.reload());
+});
+
+gulp.task('styles:main', function () {
+    gulp.src(config.files.styles.main)
+        .pipe($.plumber())
+        .pipe($.sass())
+        .pipe($.if(env === 'production', $.minifyCss()))
+        .pipe(gulp.dest(config.styles.destFolder))
+        .pipe($.connect.reload());
+});
+
+gulp.task('images', function () {
+    gulp.src(config.files.images)
+        .pipe($.plumber())
+        .pipe(gulp.dest(config.images.destFolder))
+        .pipe($.connect.reload());
+});
+
+gulp.task('scripts:libs', function () {
+    gulp.src(config.files.scripts.libs)
+        .pipe($.plumber())
+        .pipe($.concat(config.scripts.libs.outFile))
+        .pipe(gulp.dest(config.scripts.destFolder))
+        .pipe($.connect.reload());
+});
+
+gulp.task('scripts:main', function () {
+    gulp.src(config.files.scripts.main)
+        .pipe($.plumber())
+        .pipe($.concat(config.scripts.main.outFile))
+        .pipe($.if(env === 'production', $.uglify()))
+        .pipe(gulp.dest(config.scripts.destFolder))
+        .pipe($.connect.reload());
+});
+
+gulp.task('fonts', function () {
+    gulp.src(config.files.fonts)
+        .pipe($.plumber())
+        .pipe(gulp.dest(config.fonts.destFolder))
+        .pipe($.connect.reload());
+});
+
+gulp.task('build', ['templates', 'styles:libs', 'styles:main', 'images', 'scripts:libs', 'scripts:main', 'fonts']);
 
 gulp.task('watch', ['build'], function () {
     gulp.watch(config.files.templates, ['templates']);
+    gulp.watch(config.files.styles.libs, ['styles:libs']);
+    gulp.watch(config.files.styles.main, ['styles:main']);
+    gulp.watch(config.files.images, ['images']);
+    gulp.watch(config.files.scripts.libs, ['scripts:libs']);
+    gulp.watch(config.files.scripts.main, ['scripts:main']);
+    gulp.watch(config.files.fonts, ['fonts']);
 });
 
 gulp.task('server', ['build'], function() {
